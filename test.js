445bf940d28d2315e06844cec50a90965351f6d1
mabrumidev-dev <mabrumi.dev@gmail.com>
2026-08-09 22:12:32 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("URL:", process.env.SUPABASE_URL ? "OK" : "FALTOU")
+console.log("KEY:", process.env.SUPABASE_SERVICE_KEY ? "OK" : "FALTOU")
+console.log("GOOGLE:", process.env.GOOGLE_MAPS_KEY ? "OK" : "FALTOU")
